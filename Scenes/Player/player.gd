8796d1ad9ae49eae0214143676dc10570b1f4772
PingKingRing <ludwig.domberger@gmail.com>
2026-03-26 15:50:07 +0100

    Movement added

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+
+@export var SPEED = 2.0
+
+@export var Sensitivity = 0.001
+
+@onready var head = $Head
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * Sensitivity)
+		head.rotate_x(-event.relative.y * Sensitivity)
+	#if event is 
+
+func _physics_process(delta: float) -> void:
+	process_move(delta)
+
+func process_move(delta: float) -> void:
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
