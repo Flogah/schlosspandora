5fce13f3b6eee6dce830754be275ddc8c1ba70b2
Marco <professormarco77@gmail.com>
2026-03-26 15:35:08 +0100

    add door open / close / lock code

--- Scenes/door.gd
+++ Scenes/door.gd
@@ -1,13 +1,48 @@
 extends Node3D
 class_name Door
 
-var is_locked: bool = false
-var on_front: bool = false
+@onready var door_l: AnimatableBody3D = $Door_L
+@onready var door_r: AnimatableBody3D = $Door_R
 
-func lock_door(front: bool) -> void:
-	is_locked = true
-	on_front = front
+enum door_state {closed, locked_f, locked_b, open_f, open_b}
+var state
 
-func open_door(front: bool) -> void:
-	if not is_locked:
-		pass
+var is_locked: bool
+var is_closed: bool
+var lock_side_front: bool
+
+func lock_interaction(front: bool) -> void:
+	if is_locked and lock_side_front == front: is_locked = false
+	else: 
+		is_locked = true
+		lock_side_front = front
+
+func open_close_interaction(front: bool) -> void:
+	if is_closed and not is_locked:
+		is_closed = false
+		animate_opening(front)
+	else:
+		animate_closeing()
+
+func animate_locking(front: bool) -> void:
+	pass
+
+func animate_unlocking(front: bool) -> void:
+	pass
+
+func animate_opening(front: bool) -> void:
+	var tween = get_tree().create_tween()
+	var dir
+	if front:
+		dir = 90
+	else:
+		dir = -90
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(door_l, "rotation_degrees", Vector3(0, dir, 0), 1.0)
+	tween.tween_property(door_r, "rotation_degrees", Vector3(0, dir, 0), 1.0)
+
+func animate_closeing() -> void:
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(door_l, "rotation_degrees", Vector3(0, 0, 0), 1.0)
+	tween.tween_property(door_r, "rotation_degrees", Vector3(0, 0, 0), 1.0)
